691ce4bd3f472221aff0c20610b0244a36f087a4
caroline-e-miller <caroline.miller@ymail.com>
2021-04-26 17:09:49 -0500

    added index and workout models

---
+++ models/workout.js
@@ -0,0 +1,28 @@
+const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
+
+const workoutSchema = new Schema({
+
+    day: new Date(),
+
+    exercises: [
+        {
+            type: {
+                String,
+                trim: true,
+                required: "Exercise type is Required",
+            },
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number
+        }
+    ]
+})
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
